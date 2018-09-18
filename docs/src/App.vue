db42ea7cb9a9d4ed6de4ace71fac97d46b69d308
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-09-18 14:50:51 +0500

    added docs template

---
+++ docs/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <!-- If you want to highlight hardcoded source-code -->
+    <pre v-highlightjs><code class="javascript">const s = new Date().toString()</code></pre>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ docs/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import VueHighlightJS from 'vue-highlightjs'
+
+import 'bulma-fluent'
+import 'bulma-addons'
+
+Vue.use(VueHighlightJS)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
